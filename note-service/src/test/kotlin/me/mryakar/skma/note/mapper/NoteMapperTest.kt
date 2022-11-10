c162779a7dfd116caf1b50d7fbbf496f4c9d3bc3
mryakar <ahmetyakar@pm.me>
2022-11-10 19:09:47 +0300

    Add mapper tests for note-service module

--- note-service/src/test/kotlin/me/mryakar/skma/note/mapper/NoteMapperTest.kt
+++ note-service/src/test/kotlin/me/mryakar/skma/note/mapper/NoteMapperTest.kt
@@ -1,26 +1,54 @@
 package me.mryakar.skma.note.mapper
 
+import me.mryakar.skma.note.dto.NoteDto
 import me.mryakar.skma.note.entity.NoteEntity
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import org.junit.runner.RunWith
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.test.context.junit4.SpringRunner
 
 
-@RunWith(SpringRunner::class)
 class NoteMapperTest {
 
-    @Autowired
-    lateinit var mapper: NoteMapper
+    private val mapper = NoteMapper()
 
     @Test
-    fun dto_toEntity_entity() {
+    fun `entity toDto dto`() {
         val entity = NoteEntity(1, "Note")
 
         val dto = mapper.toDto(entity)
 
+        assertThat(dto.id).isEqualTo(entity.id)
+        assertThat(dto.text).isEqualTo(entity.text)
+    }
+
+    @Test
+    fun `entity list toDto dto list`() {
+        val entity = NoteEntity(1, "Note")
+
+        val dtoList = mapper.toDto(listOf(entity))
+
+        assertThat(dtoList).hasSize(1)
+        assertThat(dtoList[0].id).isEqualTo(entity.id)
+        assertThat(dtoList[0].text).isEqualTo(entity.text)
+    }
+
+    @Test
+    fun `dto toEntity entity`() {
+        val dto = NoteDto(1, "Note")
+
+        val entity = mapper.toEntity(dto)
+
         assertThat(entity.id).isEqualTo(dto.id)
         assertThat(entity.text).isEqualTo(dto.text)
+    }
+
+    @Test
+    fun `dto list toEntity entity list`() {
+        val dto = NoteDto(1, "Note")
+
+        val entityList = mapper.toEntity(listOf(dto))
+
+        assertThat(entityList).hasSize(1)
+        assertThat(entityList[0].id).isEqualTo(dto.id)
+        assertThat(entityList[0].text).isEqualTo(dto.text)
     }
 }
